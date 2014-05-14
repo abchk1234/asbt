8c7810d484f98bbb14938ef4b9676894d7e9137b
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2014-05-14 12:34:46 +0530

    Add exception for virtualbox-kernel

--- src/asbt.sh
+++ src/asbt.sh
@@ -653,6 +653,10 @@ tidy|-T)
 	for i in /var/log/packages/*_SBo*; do
 		package=$(basename "$i" | rev | cut -d "-" -f 4- | rev)
 		pkgver=$(basename "$i" | rev | cut -d "-" -f 3 | rev)
+		# Make an exception for virtualbox-kernel package
+		if [ "$package" == "virtualbox-kernel" ]; then
+			pkgver=$(echo $pkgver | cut -d "_" -f 1)
+		fi
 		path=$(find -L "$repodir" -maxdepth 2 -type d -name "$package")
 		if [ -f "$path/$package.info" ]; then
 			. "$path/$package.info"
