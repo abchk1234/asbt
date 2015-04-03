a4e47d772c2cd9814f430095e915ca4b96dc4a7b
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2015-04-03 23:56:36 +0530

    again try to fix array stuff

--- bin/asbt.sh
+++ bin/asbt.sh
@@ -464,15 +464,13 @@ check-new-pkg () {
 
 # Print the items in specified array
 print_items () {
-	local array
 	local item
-	array="$*" # array is passed as argument
-	if [ -z "$array" ]; then
+	if [[ -z $@ ]]; then
 		# No items found
 		return 1
 	else
-		# Print the array
-		for item in ${array[*]}; do
+		# Print the items
+		for item in "$@"; do
 			echo "$item"
 		done
 	fi
@@ -482,7 +480,7 @@ query-installed () {
 	local pkg=$1	# pkg to be searched for
 	# Get list of package items in /var/log/packages that match and print them
 	local items=($(find "/var/log/packages" -maxdepth 1 -type f -iname "*$pkg*" -printf "%f\n" | sort))
-	print_items "${items[*]}"
+	print_items "${items[@]}"
 }
 
 # Program options
@@ -495,7 +493,7 @@ search|-s)
 	check-config
 	check-repo
 	items=($(find -L "$repodir" -maxdepth 2 -mindepth 1 -type d -iname "*$package*" -printf "%P\n" | sort))
-	print_items "${items[*]}"
+	print_items "${items[@]}"
 	;;
 query|-q)
 	check-input "$#"
@@ -611,7 +609,7 @@ enlist|-e)
 		# ie, it checks for packages that contain the specified package in their info file, and are installed.
 		# Together they give list of packages which depend on specified package (reverse dependencies)
 		items=($($0 -e "$package" | cut -f 1 -d ":" | grep -E -w "$words" | uniq))
-		print_items "${items[*]}"
+		print_items "${items[@]}"
 	else
 		check-option "$2"
 		# The case for the package itself is skipped using -not -name in find
