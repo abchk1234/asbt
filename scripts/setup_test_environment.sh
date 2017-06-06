1f70c3d599c47ed0d60073a82fd81f4dc5693d68
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-06 14:14:01 +0530

    working on setting up test environment (2)

--- scripts/setup_test_environment.sh
+++ scripts/setup_test_environment.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # to setup environment for testing
 
+INITIAL_LOC="$(pwd)"
+
 # setup slackbuilds repo
 cd $HOME
 mkdir -p git
 cd git
 git clone --depth=1 git://git.slackbuilds.org/slackbuilds.git
-cd ..
+cd "${INITIAL_LOC}"
 
 # setup config
 mkdir -p $HOME/.config
-cp conf/asbt.conf $HOME/.config/asbt.conf
+cp aadityabagga/asbt/conf/asbt.conf $HOME/.config/asbt.conf
 
 exit $?
